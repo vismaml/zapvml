1b8eb880f2893088133038729da9c3c884851770
Hans Viken Duedal <hans.duedal@visma.com>
2019-08-06 00:50:56 +0200

    Change to module init function

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.12
 
 require (
 	github.com/blendle/zapdriver v1.1.6
+	github.com/kelseyhightower/envconfig v1.4.0
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
-	google.golang.org/grpc v1.21.0
+	google.golang.org/grpc v1.22.1
 )
--- logger.go
+++ logger.go
@@ -2,55 +2,74 @@ package zapvml
 
 import (
 	"os"
-	"sync"
 
 	"github.com/blendle/zapdriver"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+
+	"github.com/kelseyhightower/envconfig"
 	"google.golang.org/grpc/codes"
 )
 
 var (
 	// Log is global logger
-	Log *zap.Logger
-
-	// onceInit guarantee initialize logger only once
-	onceInit sync.Once
+	Log   *zap.Logger
+	Level zap.AtomicLevel
 )
 
-// Init initializes log by input parameters
+type Config struct {
+	Level zapcore.Level `required:"true" default:"warn"`
+	Debug bool          `required:"true" default:"false"`
+}
+
 func Init(globalLevel zapcore.Level) {
-	onceInit.Do(func() {
-		// High-priority output should also go to standard error, and low-priority
-		// output should also go to standard out.
-		// It is useful for Kubernetes deployment.
-		// Kubernetes interprets os.Stdout log items as INFO and os.Stderr log items
-		// as ERROR by default.
-		highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-			return lvl >= zapcore.ErrorLevel
-		})
-		lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-			return lvl >= globalLevel && lvl < zapcore.ErrorLevel
-		})
+	Level.SetLevel(globalLevel)
+}
 
-		// Output channels
-		consoleInfos := zapcore.Lock(os.Stdout)
-		consoleErrors := zapcore.Lock(os.Stderr)
+// Use package init to avoid race conditions for GRPC options
+// sync.Once still suffers from races, init functions are less complex than sync.once + waitgroup
+func init() {
+	var cfg Config
+	if err := envconfig.Process("log", &cfg); err != nil {
+		panic(err)
+	}
 
-		// Setup Config and Encoder
-		ecfg := zapdriver.NewProductionEncoderConfig()
-		consoleEncoder := zapcore.NewJSONEncoder(ecfg)
+	Level = zap.NewAtomicLevelAt(cfg.Level)
 
-		// Join the outputs, encoders, and level-handling functions into
-		// zapcore.
-		core := zapcore.NewTee(
-			zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
-			zapcore.NewCore(consoleEncoder, consoleInfos, lowPriority),
-		)
-		// From a zapcore.Core, it's easy to construct a Logger.
-		Log = zap.New(core)
-		zap.RedirectStdLog(Log)
+	// High-priority output should also go to standard error, and low-priority
+	// output should also go to standard out.
+	// It is useful for Kubernetes deployment.
+	// Kubernetes interprets os.Stdout log items as INFO and os.Stderr log items
+	// as ERROR by default.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
 	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= Level.Level() && lvl < zapcore.ErrorLevel
+	})
+
+	// Output channels
+	consoleInfos := zapcore.Lock(os.Stdout)
+	consoleErrors := zapcore.Lock(os.Stderr)
+
+	// Setup Config and Encoder
+	var ecfg zapcore.EncoderConfig
+	if cfg.Debug {
+		ecfg = zapdriver.NewDevelopmentEncoderConfig()
+	} else {
+		ecfg = zapdriver.NewProductionEncoderConfig()
+	}
+	consoleEncoder := zapcore.NewJSONEncoder(ecfg)
+
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.
+	core := zapcore.NewTee(
+		zapcore.NewCore(consoleEncoder, consoleErrors, highPriority),
+		zapcore.NewCore(consoleEncoder, consoleInfos, lowPriority),
+	)
+	// From a zapcore.Core, it's easy to construct a Logger.
+	Log = zap.New(core)
+	zap.RedirectStdLog(Log)
 }
 
 func CodeToLevel(code codes.Code) zapcore.Level {
